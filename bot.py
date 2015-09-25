5ac558bb9d624eb714c6c0cfe836bec503cef66a
kuro112 <kuro@xtcr.net>
2015-09-24 17:51:07 -0700

    new theme

--- bot.py
+++ bot.py
@@ -9,14 +9,36 @@ def index():
 
 
 
-@app.route('/settings')
-def settings():
-    return render_template("settings.html")
+@app.route('/settings-api')
+def settingsapi():
+    return render_template("settings-api.html")
 
 
 @app.route('/mybots')
 def mybots():
     return render_template("mybots.html")
+
+
+
+@app.route('/mytrading')
+def mytrading():
+    return render_template("mytrading.html")
+
+
+@app.route('/login')
+def login():
+    return render_template("login.html")
+
+
+
+@app.route('/register')
+def register():
+    return render_template("register.html")
+
+
+@app.route('/lockscreen')
+def lockscreen():
+    return render_template("lockscreen.html")
 
 
 if __name__ == '__main__':
